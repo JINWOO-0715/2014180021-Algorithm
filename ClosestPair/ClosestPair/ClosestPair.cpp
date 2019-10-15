b70c55f0c958d232cc610d7000ae32697c336f49
PJW_94 <pjw1480@naver.com>
2019-10-15 15:51:44 +0900

    알고리즘 설명 추가
    설명 추가

--- ClosestPair/ClosestPair/ClosestPair.cpp
+++ ClosestPair/ClosestPair/ClosestPair.cpp
@@ -19,11 +19,11 @@ double distance_of_point(POINT *p1, POINT *p2) // 거리 구하는 함수
 	
 	double distance = sqrt(pow(p1->x - p2->x, 2) + pow(p1->y - p2->y, 2));
 
-	if (min == -1 || distance < min)//최소 값을 저장하는데
+	if (min == -1 || distance < min)//최소 값을 저장하는데 
 	{
-		*min_pairs = *p1; // 1번 저장
-		*(min_pairs + 1) = *p2; // 2번 저장
-		min = distance; // 전역 최소거리 저장
+		*min_pairs = *p1; // 1번 점 저장
+		*(min_pairs + 1) = *p2; // 2번 점 저장
+		min = distance; // 최소 거리 저장
 	}
 
 	return distance;
@@ -47,27 +47,36 @@ double Closest_pair(POINT* p, int left, int right, int size)
 		// 구한 값중에서 최소값을 리턴하자
 		if (distance_a > distance_b)
 		{
+			printf("(%d %d) , (%d,%d) => %f \n", (p + 1)->x, (p + 1)->y, (p + 2)->x, (p + 2)->y, distance_of_point(p + 1, p + 2));
+
 			min = distance_b;
 		}
 		else
 		{
-			min = distance_a;
+			printf("(%d %d) , (%d,%d) => %f \n", p->x, p->y, (p + 1)->x, (p + 1)->y, distance_of_point(p, p + 1));
+				min = distance_a;
 		}
 
 		if (distance_b > distance_c)
 		{
+			printf("(%d %d) , (%d,%d) => %f \n",  (p + 2)->x, (p + 2)->y, (p + 0)->x, (p + 0)->y, distance_of_point(p + 2, p + 0));
 			min = distance_c;
 		}
 		else
 		{
+			printf("(%d %d) , (%d,%d) => %f \n", (p + 1)->x, (p + 1)->y, (p + 2)->x, (p + 2)->y, distance_of_point(p + 1, p + 2));
 			min = distance_b;
 		}
 		if (distance_a > distance_c)
 		{
 			min = distance_c;
+			printf("(%d %d) , (%d,%d) => %f \n", (p + 2)->x, (p + 2)->y, (p + 0)->x, (p + 0)->y, distance_of_point(p + 2, p + 0));
+
 		}
 		else
 		{
+			printf("(%d %d) , (%d,%d) => %f \n", p->x, p->y, (p + 1)->x, (p + 1)->y, distance_of_point(p, p + 1));
+
 			min = distance_a;
 		}
 		return min; // 삼각형의 변중에서 최소값을 리턴해준다. 
@@ -93,29 +102,22 @@ double Closest_pair(POINT* p, int left, int right, int size)
 			distance_range = c_left;
 		}
 
-		POINT *tmp = (POINT*)malloc(sizeof(POINT)*arr_size);// 임시 저장을 위한 포인터
+		POINT *tmp = (POINT*)malloc(sizeof(POINT)*arr_size);// 중간영역 임시 저장을 위한 포인터 
 
 		double min_sub = -1;// 최소값 저장을 위한 변수
-		int num = 0;  
+		int num = 0;  //중간 영역의 수
 
 		for (int i = left; i <= right; i++) // 
 		{
 			if (abs((p + i)->x - (p + mid)->x < distance_range)) // 절대값 p+i번째 x - 절대값 p+mid번째 x 가 범위보다 작다면.
 			{
-				(tmp + num)->x = (p + i)->x; // 임시저장변수 x = p+i의 x
-				(tmp + num)->y = (p + i)->y;// 임시저장변수 y = p+i의 y
+				(tmp + num)->x = (p + i)->x; // 임시저장변수 tmp x = p+i의 x
+				(tmp + num)->y = (p + i)->y;// 임시저장변수 tmp y = p+i의 y
 				num++;// 저장 했으니 숫자를 올린다. 
 			}
 
 		}
 		
-
-		/* 범위 내 점들 출력 */
-		printf(" 중간 범위 \n");
-		for (int i = 0; i < num; i++)
-		{
-			printf("(%d,%d) \n ", tmp[i].x, tmp[i].y);
-		}
 
 		for (int i = 0; i < num; i++) //임시 저장 값 비교 
 		{
@@ -124,6 +126,8 @@ double Closest_pair(POINT* p, int left, int right, int size)
 				if (distance_range > distance_of_point(tmp + i, tmp + j)) // 범위값 > 임시저장 변수 tmp+i번째와 tmp+j번째 
 				{
 					min_sub = distance_of_point(tmp + i, tmp + j); // 최소값은 mp+i번째와 tmp+j번째의 거리이다.
+					printf("(%d %d) , (%d,%d) => %f\n", (tmp + i)->x, (tmp + i)->y, (tmp + j)->x, (tmp + j)->y, distance_of_point((tmp + i), (tmp + j)));
+
 				}
 				else
 				{
@@ -132,33 +136,38 @@ double Closest_pair(POINT* p, int left, int right, int size)
 
 				if (min_sub < distance_range) // 만약 최소값 < 범위값이라면 
 				{
-					/* distanceRange(CPl or CPr)보다 작은 거리값이 존재한다면, */
-					/* 이를 갱신해서 좀 더 빠르게 범위를 좁혀나갈 수 있음 */
-					/* 이 과정을 거친 distanceRange가 이 부분 문제의 최근접 거리 */
 					distance_range = min_sub;// 범위값은 최소값이고
 					if (min_sub < min) // 최소값이 전역 최소값보다 작다면
 					{
 						*min_pairs = *(tmp + i); // 최근접 저장 포인터 = 임시 저장 포인터 +i번째
-						*(min_pairs + 1) = *(tmp + j); // 그
+						*(min_pairs + 1) = *(tmp + j); // 최근접 저장 포인터 2번째점은 *(tmp + j)
 					}
 				}
 			}
 
 		}
-
+		
 		if (min_sub == -1)
 		{
 			printf("범위 내 CPc가 존재하지 않습니다.\n");
 		}
 		else
 		{
-			printf("CPc : %lf\n\n", min_sub);
+				printf("minsub : %lf\n\n", min_sub);
 		}
 
-		/* 동적 할당된 메모리 해제 */
-		free(tmp);
 
-		/* 최근접 거리 리턴 */
+		printf(" 중간 범위 \n");
+		printf("distance_range : %lf\n", distance_range);
+		for (int i = 0; i < num; i++)
+		{
+
+			printf("(%d,%d)", tmp[i].x, tmp[i].y);
+
+		}
+		printf("\n");
+
+		free(tmp);
 		return distance_range;
 	}
 
@@ -201,39 +210,40 @@ void quick_sort(POINT p[], int left, int right) // 퀵정렬
 }
 int main()
 {
-	POINT * pair = NULL;// 점을 입력 받을 변수
+	POINT * pair = NULL;// 점을 입력 받을 포인터
 
-	pair = (POINT*)malloc(sizeof(POINT)*arr_size); // 점 동적할당
+	pair = (POINT*)malloc(sizeof(POINT)*arr_size); // 포인터 동적할당
 
 	min_pairs = (POINT*)malloc(sizeof(POINT)*arr_size * 2); // 최접점쌍 저장 동적할당.
 
-	POINT point[arr_size] = { {316, 353}, {253, 857}, {743, 851}, {183, 998},
-	{262, 439}, {330, 732},
-	{418, 232}, {856, 448}, {596, 658}, {266, 100}, {890, 793}, {665, 864}, {238, 657}, {636, 731}, {735, 606},
-	{562, 203}, {492, 701}, {306, 393}, {901, 902}, {921, 900}, {637, 780}, {620, 975}, {718, 94}, {887, 749}, {756, 560},
-	{525, 805}, {229, 29}, {352, 570}, {670, 589}, {644, 821} };
+	POINT point[arr_size] = { {316, 353}, {253, 857}, {743, 851}, {183, 998}, {262, 439}, {330, 732}, {418, 232}, {856, 448},
+	{596, 658}, {266, 100}, {890, 793}, {665, 864}, {238, 657}, {636, 731}, {735, 606}, {562, 203}, {492, 701}, {306, 393},
+	{798, 939}, {921, 176}, {637, 780}, {620, 975}, {718, 94}, {887, 749},
+	{756, 560}, {525, 805}, {229, 29}, {352, 570}, {670, 589}, {644, 821} }; // 점 입력 
 
-	printf(" x순서 정렬 전\n ");
-	for (int i = 0; i < 30; i++)
-	{
-		printf("%d %d\n", point[i].x, point[i].y);
-	}
-	quick_sort(point, 0, arr_size - 1);
+	quick_sort(point, 0, arr_size - 1); // 점 퀵소트로 정렬 
 	
 	printf(" x순서 퀵정렬 후\n ");
-
+	int k = 0;
 	for (int i = 0; i < 30; i++)
 	{
-		printf("%d %d\n", point[i].x, point[i].y);
-	}
+		k++;
+		printf(" (%d %d) ", point[i].x, point[i].y);
+		if (k % 10 == 1)
+		{
+			printf("\n ");
+		}
+	} // 정렬 출력 
 	for (int i = 0; i < arr_size; i++)
 	{
 		pair[i].x = point[i].x;
 		pair[i].y = point[i].y;
 	}
+	printf("\n ");
 
-	printf("최근접 거리 : %lf\n", Closest_pair(pair, 0, 30 - 1, 30));
-	for (int i = 0; i < 2; i++)
+	printf("최근접 거리 : %lf\n", Closest_pair(pair, 0, 30 - 1, 30)); // 최근접점 이웃쌍 찾기
+
+	for (int i = 0; i < 2; i++) // 최근접점 이웃쌍 출력
 	{
 		printf("(%d, %d)\n", (min_pairs + i)->x, (min_pairs + i)->y);
 	}
